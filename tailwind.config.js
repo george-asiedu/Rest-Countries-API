4c21a707a91f64afbe0fdb38d1d87e6a609e0d86
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-06-11 17:17:03 +0000

    tailwind path configured

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      darkNavy: '#2B3844',
+      veryDarkNavy: '#202C36',
+      black: '#111517',
+      lightBackground: '#979797'
+    },
+  },
+  plugins: [],
+}

e3f201adabafee4d95808d3d6606d389a0123c80
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-06-27 14:08:06 +0000

    ngx spinner, useFetch composable logic reused in the home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,35 +3,51 @@ import { RouterModule } from '@angular/router';
 import { Countries } from '../../model/countries';
 import { CountryService } from '../../service/country.service';
 import { FormsModule } from '@angular/forms';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { NgFor, NgIf } from '@angular/common';
+import { NgxSpinnerService, NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, FormsModule],
+  imports: [
+    RouterModule, 
+    FormsModule, 
+    CountryListComponent, 
+    NgFor, 
+    NgIf,
+    NgxSpinnerModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    public countryList: Countries[] = []
-    public allCountries: Countries[] = []
-    public isLoading: boolean = true
-    public error: string = ''
-    public search: string = ''
-    public selectedRegion: string = ''
+  public countries: Countries[] = []
+  public allCountries: Countries[] = []
+  public isLoading: boolean = false
+  public error: string = ''
+  public search: string = ''
+  public selectedRegion: string = ''
 
-    constructor(private cs: CountryService) {}
+  constructor(private cs: CountryService, private ngxSpinner: NgxSpinnerService) {}
 
-    ngOnInit(): void {
-        this.cs.getAllCountries().subscribe({
-          next: (response) => {
-            this.countryList = response
-            this.allCountries = response
-            this.isLoading = false
-          },
-          error: (err: string) => {
-            this.error = err
-          }
-      })
+  ngOnInit(): void {
+    this.isLoading = true
+    this.ngxSpinner.show()
+
+    this.cs.fetchCountries.retry().then(() => {
+      const fetchData = this.cs.fetchCountries.data()
+      if (fetchData !== null) {
+        this.countries = fetchData
+        this.allCountries = fetchData
+        this.isLoading = false
+        this.ngxSpinner.hide()
+      }
+    }).catch(error => {
+      this.error = error.message
+      this.isLoading = false
+      this.ngxSpinner.hide()
+    })
   }
 
   countrySearch(): void {
@@ -45,19 +61,19 @@ export class HomeComponent implements OnInit {
   filterCountries(): void {
     let filteredCountries = this.allCountries
 
-    if(this.selectedRegion) {
+    if (this.selectedRegion) {
       filteredCountries = filteredCountries.filter(
         country => country.region === this.selectedRegion
       )
     }
 
-    if(this.search)  {
+    if (this.search) {
       const searchTerm: string = this.search.toLowerCase()
       filteredCountries = filteredCountries.filter(
         country => country.name.toLowerCase().includes(searchTerm)
-      )
+      );
     }
 
-    this.countryList = filteredCountries
+    this.countries = filteredCountries
   }
 }

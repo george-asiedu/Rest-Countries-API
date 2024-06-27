6ba0c87a2c5aaa4b3d90320e9913d7c306045bd9
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-06-27 14:06:39 +0000

    ngx spinner added, and useFetch composables reused in the details component

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -3,42 +3,49 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Countries } from '../../model/countries';
 import { CountryService } from '../../service/country.service';
 import { NgFor, NgIf } from '@angular/common';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [RouterModule, NgIf, NgFor],
+  imports: [RouterModule, NgIf, NgFor, NgxSpinnerModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent implements OnInit {
   public countryDetails: Countries = {} as Countries
   public isLoading: boolean = false
+  public error: string = ''
 
-  constructor(private cs: CountryService, private ar: ActivatedRoute) {}
+  constructor(private cs: CountryService, private ar: ActivatedRoute, private ngxSpinner: NgxSpinnerService) {}
 
   ngOnInit(): void {
-      this.ar.paramMap.subscribe(
-        (param) => {
-          let name: string | null = param.get('name')
-          if(name !== null) this.getCountryDetails(name)
-        }
-      )
+    this.ar.paramMap.subscribe(
+      (param) => {
+        let name: string | null = param.get('name')
+        if(name !== null) this.getCountryDetails(name)
+      }
+    )
   }
 
   getCountryDetails(name: string) {
     this.isLoading = true
-    this.cs.getAllCountries().subscribe({
-      next: (response: Countries[]) => {
-        const country = response.find(country => country.name === name)
-        if(country) this.countryDetails = country
-        this.isLoading = false
-      },
-      error: () => {
-        console.error('No country displayed')
-        this.isLoading = false
+    this.ngxSpinner.show()
+
+    this.cs.fetchCountries.retry().then(() => {
+      const fetchData = this.cs.fetchCountries.data()
+      if(fetchData !== null) {
+        const country = fetchData.find(country => country.name === name)
+        if(country) {
+          this.isLoading = false
+          this.countryDetails = country
+          this.ngxSpinner.hide()
+        }
       }
-    }
-    )
+    }).catch(error => {
+      this.error = error.message
+      this.isLoading = false
+      this.ngxSpinner.hide()
+    })
   }
 }
